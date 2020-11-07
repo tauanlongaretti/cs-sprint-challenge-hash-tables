2972ff284503c36f2a10483503159f21b82488bf
tauan-longaretti <tauanlongaretti@gmail.com>
2020-11-06 23:50:29 -0500

    mvp

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,13 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    data = {}
+        
+    for item in range(length):
+        data[weights[item]] = item
 
+    for item in range(length):
+        if limit - weights[item] in data:
+            return (max(item, data[limit - weights[item]]), min(item, data[limit - weights[item]]))
+            
     return None
